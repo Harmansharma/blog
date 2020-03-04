f8be830054f3388b1d244e3ebb898c48b7b97261
harman sharma <sharmaharman539@gmail.com>
2020-03-04 11:01:30 +0530

    modified app/uploaders/image_uploader.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,10 @@
 class Article < ApplicationRecord
 	has_many :suggests
+
 	validates :title,
 	 presence: true,
 	 length: {minimum: 6}
+
+	 mount_uploader :image, ImageUploader
          
    end                 
